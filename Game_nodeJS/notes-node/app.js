22147c7e42169459995fc76e954ae4eb22039826
Peter Krak <pkrak@csas.cz>
2018-04-07 15:51:34 +0200

    last update
    last update

--- Game_nodeJS/notes-node/app.js
+++ Game_nodeJS/notes-node/app.js
@@ -2,17 +2,24 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require ('lodash');
 const notes = require ('./notes.js');
 
-var user;
-user = os.userInfo();
-var res = notes.addnotes();
-console.log(res);
+
+//var user;
+//user = os.userInfo();
+//var res = notes.addnotes();
+//console.log(res);
 
 console.log('return:' + notes.add(9,2));
+console.log(_.isString('Gary'));
+console.log(_.isString(true));
+
+var filteredArray = _.uniq(['Gary',1,1,1,2,'Gary',2,1,2,3,4,5,5,'Garz','Gary' ]);
+console.log(filteredArray);
 
 
 //fs.appendFileSync('greetings.txt','Hello ' + user.username + '! ');
-fs.appendFileSync(`greetings.txt`,`Hellos ${user.username} Jsem ${notes.age} let starý`);
+//fs.appendFileSync(`greetings.txt`,`Hellos ${user.username} Jsem ${notes.age} let starý`);
 
 console.log('end of run');

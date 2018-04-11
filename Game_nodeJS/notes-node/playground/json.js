2c9e8d3ad692be8c0e5aae9be13950cc60698766
Peter Krak <peter.krak@centrum.cz>
2018-04-11 22:35:53 +0200

    ,
    ,

--- Game_nodeJS/notes-node/playground/json.js
+++ Game_nodeJS/notes-node/playground/json.js
@@ -6,3 +6,10 @@ var stringObj = JSON.stringify(obj);
 
 console.log(typeof stringObj);
 console.log(stringObj);
+
+var personString = '{"name": "Andrew", "age": 25}';
+
+var person = JSON.parse(personString);
+
+console.log(typeof person);
+console.log(person);

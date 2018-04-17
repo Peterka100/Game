4c8e7ccedc0f1025391a792ab6d296d46ad88f48
Peter Krak <pkrak@csas.cz>
2018-04-17 16:09:49 +0200

    try catch
    try catch

--- Game_nodeJS/notes-node/notes.js
+++ Game_nodeJS/notes-node/notes.js
@@ -13,8 +13,12 @@ var addNote = function (title, body) {
         body
     };
 
-    var noteString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(noteString);
+    try {
+        var noteString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(noteString);
+    } catch (e) {
+
+    }
 
     notes.push(note);
     fs.writeFileSync('notes-data.json', JSON.stringify(notes));

2288029f683b60782444b979619a29d549ecb844
Peter Krak <pkrak@csas.cz>
2018-04-18 16:12:35 +0200

    .
    .

--- Game_nodeJS/notes-node/notes.js
+++ Game_nodeJS/notes-node/notes.js
@@ -7,27 +7,33 @@ var getAll = function () {
 
 var addNote = function (title, body) {
     console.log('Adding note: ',title, body);
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
 
+
+    /*
     try {
         var noteString = fs.readFileSync('notes-data.json');
         notes = JSON.parse(noteString);
     } catch (e) {
 
     }
+    */
 
-    var duplicateNote = notes.filter(function (note) {
-        return note.title === title;
-    }) ;
+        var duplicateNote = notes.filter(function (note) {
+            return note.title === title;
+        }) ;
 
-    if (duplicateNote.length === 0) {
-        notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-    }
+
+        if (duplicateNote.length === 0) {
+            notes.push(note);
+            saveNotes(notes);
+        }
+
+
 };
 
 var getNote = function(title){
@@ -37,6 +43,21 @@ var getNote = function(title){
 var removeNote = function(title){
     console.log('Removing note: ', title);
 };
+
+var fetchNotes = function () {
+    try {
+        var noteString = fs.readFileSync('notes-data.json');
+        return JSON.parse(noteString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = function (notes) {
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+
 
 module.exports = {
     addNote: addNote,

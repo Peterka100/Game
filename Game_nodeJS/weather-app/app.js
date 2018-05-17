a0582c58b14d6aebf2e86d5bee15c6fec89e2691
Peter Krak <pkrak@csas.cz>
2018-05-17 12:18:11 +0200

    encoded adress
    encoded address

--- Game_nodeJS/weather-app/app.js
+++ Game_nodeJS/weather-app/app.js
@@ -15,7 +15,9 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
+var  encodedAddress = encodeURIComponent(argv.address);
 console.log(argv);
+console.log(encodedAddress);
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',

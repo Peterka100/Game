c527b9dcad3baf8c648d24dbbdb33b27b3f8b846
Peter Krak <pkrak@csas.cz>
2018-05-17 09:40:00 +0200

    Definition of argv
    Definition of argv

--- Game_nodeJS/weather-app/app.js
+++ Game_nodeJS/weather-app/app.js
@@ -2,17 +2,31 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
+
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
         //  url: 'http://www-csas.csint.cz/webapi/v1/info/branches?web-api-key=dd3c4d08-6c39-411a-a37d-0fbea365fc1e',
         json: true
 },
     function (error, response, body) {
-        console.log('body: ', JSON.stringify(body, undefined, 2));
-        console.log('error: ', error);
-        console.log('response: ', JSON.stringify(response, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+ //       console.log('body: ', JSON.stringify(body, undefined, 2));
+ //       console.log('error: ', error);
+ //       console.log('response: ', JSON.stringify(response, undefined, 2));
+          console.log(`Address: ${body.results[0].formatted_address}`);
+          console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+          console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
-

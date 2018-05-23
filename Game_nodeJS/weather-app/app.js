a0075c61041a23250d098e0ad0d27875ea58b612
Peter Krak <pkrak@csas.cz>
2018-05-23 16:11:30 +0200

    .
    .

--- Game_nodeJS/weather-app/app.js
+++ Game_nodeJS/weather-app/app.js
@@ -1,9 +1,20 @@
-// GEOCODE API
 
-/*
+
+
+
 const yargs = require('yargs');
-
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+weather.getWeather(39.9396284, -75.18663959999999, function(errorMessage, weatherResults) {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(weatherResults, undefined, 2));
+    }
+});
+
+
 
 const argv = yargs
     .options({
@@ -18,6 +29,8 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
+/*
+
 geocode.geocodeAddress(argv.address, function(errorMessage, results){
     if (errorMessage) {
         console.log(errorMessage);
@@ -26,7 +39,7 @@ geocode.geocodeAddress(argv.address, function(errorMessage, results){
     }
     });
 
-*/
+
 
 const request = require('request');
 request({
@@ -42,7 +55,7 @@ request({
     }
 });
 
-
+*/
 
 
 // console.log(argv);
--- Game_nodeJS/weather-app/weather/weather.js
+++ Game_nodeJS/weather-app/weather/weather.js
@@ -1,15 +1,23 @@
 const request = require('request');
 
+var getWeather = function (lat, lng, callback) {
+    request({
+        url: `https://api.darksky.net/forecast/d952ca7f505fbf828c35bb1196ede40f/${lat},${lng}`,
+        json: true
+    },function (error, response, body) {
+        if(error){
+            callback("Unable to connect to Forecast.io server.");
+        }else if (response.status.code === 400) {
+            callback('Unable to fetch weather.');
+        }else if (response.status.code === 200) {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature
+            });
+        }
+    });
+};
 
-request({
-    url: 'https://api.darksky.net/forecast/d952ca7f505fbf828c35bb1196ede40f/39.9396284,-75.18663959999999',
-    json: true
-},function (error, response, body) {
-    if(error){
-        console.log("Unable to connect to Forecast.io server.");
-    }else if (response.status.code === 400) {
-        console.log('Unable to fetch weather.');
-    }else if (response.status.code === 200) {
-        console.log(body.currently.temperature);
-    }
-});
+module.exports.getWeather = getWeather;
+
+

03909ef257e732fbdd9f337ad42f91ed849629d5
Peter Krak <pkrak@csas.cz>
2018-05-25 15:25:51 +0200

    .
    .

--- Game_nodeJS/weather-app/playground/promise.js
+++ Game_nodeJS/weather-app/playground/promise.js
@@ -1,3 +1,7 @@
 var somePromise = new Promise(function (resolve, reject) {
-        resolve ("Hey, It works");
+    resolve ("Hey, It works");
+});
+
+somePromise.then(function (message) {
+    console.log("Success: ", message);
 });

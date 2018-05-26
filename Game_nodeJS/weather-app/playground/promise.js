d7bbc4b6277fb168dfe10110d7b70548f93fabfd
Peter Krak <peter.krak@centrum.cz>
2018-05-26 08:49:51 +0200

    promise
    promise

--- Game_nodeJS/weather-app/playground/promise.js
+++ Game_nodeJS/weather-app/playground/promise.js
@@ -1,7 +1,12 @@
 var somePromise = new Promise(function (resolve, reject) {
-    resolve ("Hey, It works");
+    setTimeout(function () {
+        //resolve ("Hey, It works");
+        reject("Unable to fullfil promise")
+    },2500)
 });
 
 somePromise.then(function (message) {
     console.log("Success: ", message);
-});
+},function (errorMessage) {
+    console.log(errorMessage);
+    });

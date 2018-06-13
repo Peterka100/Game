5d849803005952e9b8951f0d9f395f1a018d1eeb
Peter Krak <pkrak@csas.cz>
2018-06-13 15:00:19 +0200

    ...
    ..

--- Game_nodeJS/weather-app/weather/app-promise.js
+++ Game_nodeJS/weather-app/weather/app-promise.js
@@ -13,3 +13,27 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeURL).then(function(response) {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address.');
+    }
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    console.log(response.data);
+}).catch (function(e) {
+    if (e.code === 'ENOTFOUND'){
+        console.log('Unable to connect to API servers.');
+    }  else {
+        console.log(e.message);
+    }
+});
+
+
+
+

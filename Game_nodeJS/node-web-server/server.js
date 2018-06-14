7a4a3d7032d0d9f2ffd22d8334066de5cc0b3dc2
Peter Krak <pkrak@csas.cz>
2018-06-14 14:20:25 +0200

    server hbs
    server hbs

--- Game_nodeJS/node-web-server/server.js
+++ Game_nodeJS/node-web-server/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+// Mělo by to nasetovat cestu k adresaru, kde máme HTML stránky
+app.set('view engine', 'hbs');
+
+//Príprava templete
+app.use(express.static(__dirname + '/public'));
+
+
+
+//Nastavovaní jednotlivých route
 app.get('/', function (req,res) {
 //  res.send('<h1>Hello Express!</h1>');
     res.send({
@@ -10,7 +20,23 @@ app.get('/', function (req,res) {
             'Biking',
             'Cities'
         ]
-    })
+    });
 });
 
-app.listen(3000);
+
+app.get('/about', function (req,res) {
+   res.render('about.hbs', {
+       pageTitle: 'About Page',
+       currentYear: new Date().getFullYear()
+   });
+});
+
+app.get('/bad', function (req,res) {
+   res.send({
+      errorMessage: 'Unable to handle request'
+   });
+});
+
+app.listen(3000, function () {
+    console.log('Server i up on port 3000');
+});

2111e80e4526e6c48c39e7b4c989417873e893ad
Peter Krak <pkrak@csas.cz>
2018-06-27 13:41:57 +0200

    .
    .

--- Game_nodeJS/node-web-server/server.js
+++ Game_nodeJS/node-web-server/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
@@ -10,8 +11,8 @@ hbs.registerPartials(__dirname + '/views/partials');
 // Mělo by to nasetovat cestu k adresaru, kde máme HTML stránky
 app.set('view engine', 'hbs');
 
-//Príprava templete
-app.use(express.static(__dirname + '/public'));
+// Príprava template
+
 app.use(function (req, res, next) {
     var now = new Date().toString();
     var log = `${now}: ${req.method}  ${req.url}`;
@@ -27,8 +28,11 @@ app.use(function (req, res, next) {
 });
 
 app.use(function(req, res, next){
-   res.render('maintenance.hps');
+   res.render('maintenance.hbs');
+   next();
 });
+
+app.use(express.static(__dirname + '/public'));
 
 //registrace helpru
 hbs.registerHelper('currentYear', function () {
@@ -70,5 +74,66 @@ app.get('/bad', function (req,res) {
 });
 
 app.listen(3000, function () {
-    console.log('Server i up on port 3000');
+    console.log('Server is up on port 3000');
+});
+
+*/
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use(function(req, res, next) {
+    var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`;
+console.log(log);
+fs.appendFile('server.log', log + '\n', function(err) {
+    if (err) {
+        console.log('Unable to append to server.log.');
+    }
+});
+next();
+});
+
+app.use(function(req, res, next) {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', function() {
+    //return 'test';
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', function(text) {
+    return text.toUpperCase();
+});
+
+app.get('/', function(req, res) {
+    res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+});
+});
+
+app.get('/about', function(req, res) {
+    res.render('about.hbs',{
+    pageTitle: 'About Page'
+});
+});
+
+app.get('/bad', function(req, res) {
+    res.send({
+    errorMessage: 'Unable to handle request'
+});
+});
+
+app.listen(3000, function() {
+    console.log('Server is up on port 3000');
 });
